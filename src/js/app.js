61eb1af5964e238491efa810ba49a347c303a302
Bart Wttewaall <bart@wirelab.nl>
2020-06-06 18:08:50 +0200

    Tested build process Added missing dependencies Set up bootstrap build Added prettier and eslint configs Added required folders in src/assets with gitkeep files

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// import {} from 'bootstrap';
 
-import "../scss/app.scss";
+import '../scss/app.scss';
+
+// import initMyComponent from "./components/my-component";
+
+document.addEventListener('DOMContentLoaded', () => {
+  // initMyComponent();
+});
